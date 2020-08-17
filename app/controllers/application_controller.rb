17134a7693423fbd0e7d130c17af9a8538ca3724
Patrick McKernin <patrick@firstdraft.com>
2020-08-17 13:51:48 -0500

    added a before action in the app controller to prevent stale cookies from causing an error

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,19 @@
 class ApplicationController < ActionController::Base
+  before_action(:reset_stale_session)
   before_action(:load_current_user)
+
+  def reset_stale_session
+    current_user_id = session.fetch(:user_id)
+
+    if current_user_id != nil
+      current_user = User.where({ :id => current_user_id }).at(0)
+      
+      if current_user == nil
+        reset_session
+        redirect_to("/", { :alert => "Whaaaaattt? How did I end up here? Your current user ID was stale (likely due to a sample data reset), so instead of causing an error we deleted your cookies and brought you back to the homepage. Please sign in again." })
+      end
+    end
+  end
   
   def load_current_user
     the_id = session.fetch(:user_id)
